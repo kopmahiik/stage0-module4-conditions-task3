4479bbd972561912e78aaf043e4757d3da1eb208
Aziz Ulmasov <azizulmasov93@gmail.com>
2023-07-03 17:51:35 +0500

    Task completed

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month >= 1 && month <= 12) {
+            if (month % 2 == 0 && month != 8) {
+                if (month == 2) {
+                    System.out.println(28);
+                } else {
+                    System.out.println(30);
+                }
+            } else {
+                System.out.println(31);
+            }
+        } else {
+            System.out.println("wrong number!");
+        }
     }
 }
